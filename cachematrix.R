b9d0ad04b2cbe5626d2507d20eb54670d033ec99
Todd Wight <metoddwight@gmail.com>
2015-02-21 21:45:00 -0800

    added initials, testing push

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,8 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
+##tbw test
+
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
